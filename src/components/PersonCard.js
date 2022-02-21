3e8e9c362de9df70e90298beabeea1d8ad26641b
Sabrina Henrice <sabrinahenrice@gmail.com>
2022-02-20 19:15:18 -0500

    quick assignment

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState} from 'react';
 const PersonCard = (props) => {
+    const [age, setAge] = useState (props.age)
     return(
         <div>
             <h1>{ props.lastName }, { props.firstName }</h1>
-            <p>Age: { props.age }</p>
+            <p>Age: { age }</p>
             <p>Hair Color: { props.hairColor }</p>
+            <button onClick={(event) => setAge(age + 1)}>It's {props.firstName}'s birthday</button>
         </div>
     );
 }
